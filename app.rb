06f796ff09e873ed75b1e92404b203e4df1c3ba7
github username <github email address>
2019-04-22 22:05:24 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+#GET /newteam renders basketball team form
+  get '/newteam' do
+    erb :newteam
+  end
+#POST /team displays the basketball team details in the browser
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
 
 end
